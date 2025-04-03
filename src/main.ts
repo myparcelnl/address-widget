c2f75bf520d6f1a59ac0dfb14b3408fe3417c954
Joeri van Veen <joeri@myparcel.nl>
2025-04-03 16:16:07 +0200

    fix: have a working solution for t

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 /**
  * This is the main module file used to export the Vue app and any functionality that should be available to the consuming application.
  */
+import {createApp} from 'vue';
+import i18n from "@/i18n.ts";
 import App from './App.vue';
 import {ADDRESS_SELECTED_EVENT} from './composables/useOutgoingEvents';
 import {CONFIGURATION_UPDATE_EVENT} from './composables/useIncomingEvents';
 
-export default App;
+const app = createApp(App);
+
+app.use(i18n);
+
+export default app;
 
 // Additional exports for typing etc.
 export {ADDRESS_SELECTED_EVENT, CONFIGURATION_UPDATE_EVENT};
