df023154a024ab252412d668e7d2c3139b0b9ad1
Freek van Rijt <freek.vanrijt@myparcel.nl>
2025-04-09 13:47:05 +0200

    ci: fix using incorrect git credentials
    fix an issue where incorrect git credentials were being used for the commit plugin

--- release.config.cjs
+++ release.config.cjs
@@ -21,7 +21,7 @@ module.exports = {
     addReleaseNotesGeneratorPlugin(),
     addChangelogPlugin(),
     addNpmPlugin(),
-    addGitHubPlugin(),
-    addGitPlugin()
+    addGitPlugin(),
+    addGitHubPlugin()
   ],
 };
